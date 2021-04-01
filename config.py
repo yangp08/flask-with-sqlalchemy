30769b1be13e27994ce6e42b70de4dc19e273f05
yangp08 <petchengyang@yahoo.fr>
2021-04-01 12:11:56 +0200

    fix heroku postgres bug with DATABASE_URL_CUSTOM in config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL_CUSTOM']
